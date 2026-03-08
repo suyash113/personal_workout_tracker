f75ddda1274b37a09523d0e6afe3c231788a82dd
suyash_parve <suyashparve113sp@gmail.com>
2026-03-08 14:08:48 +0530

    populated back-biceps page

--- script.js
+++ script.js
@@ -17,7 +17,52 @@ const today = new Date().toLocaleDateString('en-US', {
 // Inject it
 dateDisplay.innerText = today;
 
-backBiceps.addEventListener("click", function() {
-    workoutStage.innerHTMl = ""
-    workoutStage.innerHTMl = '' 
-})
+backBiceps.addEventListener("click", BackBiceps )
+
+function BackBiceps () {
+    workoutStage.innerHTML = `<div class="entire-page">
+        <div class="all-exercises">
+            <i class="fa-sharp fa-solid fa-arrow-left" id="back-btn"></i>
+            <div class="backHeading-date">
+                <h1>BACK & BICEPS SESSION</h1>
+                <p id="current-date"></p>
+            </div>
+        </div>
+        <div class="main-exercise">
+
+        </div>
+        <h3 id="add-exer">+Add exercise</h3>
+        <div class="modal" id="modal">
+            <i class="fa-regular fa-trash-can" id="delete-exer-btn"></i>
+            <div id="modal-inner">
+            
+            </div>
+        </div>
+    </div>`
+
+    document.getElementById('add-exer').addEventListener("click", newExercise)
+
+
+}
+
+
+function newExercise () {
+    const modal = document.getElementById('modal')
+    let modalInner = document.getElementById('modal-inner')
+    modalInner.innerHTML = `
+                <h2>Add new exercise</h2>
+                <p>Create a new exercise to track your progress</p>
+
+                <div class='form'>
+                    <label for="exercise-name">Exercise name</label>
+                    <input type="text" id="exercise-name" placeholder="Lat-pulldown" required>
+                    <label for="goal">Goal</label>
+                    <input type="text" id="goal" placeholder="e.g., 3-4 sets progressive overload">
+                    <button id="submit">Add Exercise</button>
+                </div>
+                `
+    
+    modal.style.display = 'flex'
+
+    
+}
